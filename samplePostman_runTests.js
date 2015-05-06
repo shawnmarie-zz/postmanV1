f41f4b17f1f380982d1ef8977ab57425303ad648
Josh Gough <jsgough@gmail.com>
2015-05-06 11:24:41 -0400

    Use commander.js for command-line options parsing, including RegExp pattern for story OIDs.

--- samplePostman_runTests.js
+++ samplePostman_runTests.js
@@ -1,15 +1,36 @@
+#!env node
+
 var Newman = require('newman'),
   FS = require('q-io/fs'),
   fsNode = require('fs'),
   JSON5 = require('json5'),
-  baseApiUrl = 'https://www14.v1host.com/v1sdktesting/rest-1.v1/Data/',
-  baseHostUrl = 'https://www14.v1host.com',
   _ = require('lodash'),
-  v1hal = require('./lib/v1hal')(baseHostUrl),
-  util = require('util');
-_.mixin(require('lodash-deep'));
+  util = require('util'),
+  program = require('commander');
 
-// OLD:
+program
+  .version('0.0.1')
+  .option('-i --instanceUrl <instanceUrl>', 'Set the V1 instance URL to use', 'https://www14.v1host.com/v1sdktesting')
+  .option('-u --user <user:password>', 'Set the V1 instance user credentials', 'admin:admin')
+  .option('-a --assetPattern <assetPattern>',  'Set the RegExp pattern to use for extracting the V1 asset OID from the Postman test title', '[A-Z,a-z]+:[0-9]+')
+  .option('-c --configFileName <configFileName>', 'Set the filename to store the mappings of Postman test IDs to V1 asset OIDs', 'postmanTestsToV1AssetMap.json')
+  .parse(process.argv);
+
+var instanceUrl = program.instanceUrl;
+var user = program.user;
+var assetPattern = program.assetPattern;
+var configFileName = program.configFileName;
+
+// ex: https://www14.v1host.com/v1sdktesting
+if (instanceUrl[instanceUrl.length] !== '/') instanceUrl += '/';
+var baseApiUrl = instanceUrl + 'rest-1.v1/Data/';
+// ex: baseApiUrl = 'https://www14.v1host.com/v1sdktesting/rest-1.v1/Data/',    
+var baseHostUrl = instanceUrl.substr(0, instanceUrl.lastIndexOf('/'));
+// ex: baseHostUrl = 'https://www14.v1host.com',
+
+console.log('Using V1 instance at: ' + instanceUrl);
+
+var v1hal = require('./lib/v1hal')(baseHostUrl, user);
 
 // read the collectionjson file
 var collectionJson = JSON5.parse(fsNode.readFileSync('HACK.json', 'utf8'));
@@ -23,17 +44,20 @@ newmanOptions = {
   stopOnError: true
 }
 
-// Optional Callback function which will be executed once Newman is done executing all its tasks.
 
-var mappingFileName = 'postmanTestsToV1AssetMap.json';
+function getAssetOid(str) {
+  var matches = str.match(assetPattern);
+  if (matches && 0 < matches.length) return matches[0];
+  return '';      
+}
 
-FS.isFile(mappingFileName)
+FS.isFile(configFileName)
   .then(function(exists) {
-    if (!exists) return FS.write(mappingFileName, '{}');
+    if (!exists) return FS.write(configFileName, '{}');
     return true;
   })
   .then(function() {
-    return FS.read(mappingFileName);
+    return FS.read(configFileName);
   })
   .then(function(json) {
     json = json || '{}';
@@ -42,7 +66,8 @@ FS.isFile(mappingFileName)
     Newman.addEventListener("iterationRunnerResultsAvailable", function(response) {
       var map = {};
       _.forEach(response.results, function(item) {
-        map[item.id] = item.name;
+        var name = getAssetOid(item.name);
+        map[item.id] = name;
       });
 
       _.forEach(map, function(storyNumber, postmanTestId) {
@@ -53,7 +78,7 @@ FS.isFile(mappingFileName)
             Name: storyNumber
           })
             .then(function(res) {
-              console.log("Successfully updated the V1 Test asset:");
+              console.log("Successfully updated existing V1 Test asset:");
               console.log(res.data);
             })
             .catch(function(err) {
@@ -74,12 +99,12 @@ FS.isFile(mappingFileName)
               var id = res.data._links.self.id;
               var momentLessId = id.substr(0, id.lastIndexOf(':'));
               postmanTestsToV1AssetMap[postmanTestId] = momentLessId;
-              console.log('New asset:');
+              console.log('Created new V1 Test asset:');
               console.log(res.data);
-              return FS.write(mappingFileName, JSON.stringify(postmanTestsToV1AssetMap))
+              return FS.write(configFileName, JSON.stringify(postmanTestsToV1AssetMap))
             })
             .then(function() {
-              console.log('Updated ' + mappingFileName + ' with new mapping.');
+              console.log('Updated ' + configFileName + ' with new mapping.');
             })
             .catch(function(error) {
               console.error("Error in creating V1 Test asset after Postman test run:");
@@ -95,15 +120,3 @@ FS.isFile(mappingFileName)
   .catch(function(err) {
     console.error(err);
   });
-
-/*
-  v1hal.get(baseApiUrl + "Test?where=Parent='Story:6150'&sel=ID")
-  .then(function(res) {
-    var idrefs = _.deepPluck(res.data, '_links.ID.idref');
-    console.log(idrefs);
-  })
-  .catch(function(err) {
-    console.error("Error getting Test:");
-    console.error(err);
-  });  
-  */
